59fb1af2595ac07770ffa6f0a84801f405510908
ndkhili <nourelimen.dkhili@gmail.com>
2021-04-09 17:26:50 +0200

    feat(UnitTest): Initiating Test Project

---
+++ Calculator.Services.Tests/CalculateServicesTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using RPNCalculator.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Services.Tests
+{
+    public class CalculateServicesTest
+    {
+        private ICalculateService calculateService;
+
+        public CalculateServicesTest(ICalculateService calculateService)
+        {
+            this.calculateService = calculateService;
+        }
+
+        [Test]
+        public void Calculate_Should_Succedd()
+        {
+        }
+    }
+}
---
+++ Calculator.Services.Tests/DataTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Services.Tests
+{
+    public static class DataTest
+    {
+        public const int CAPACITY = 4;
+
+        public static Stack<decimal> GetStack()
+        {
+            Stack<decimal> stack = new Stack<decimal>();
+
+            stack.Push(1);
+            stack.Push(4);
+            stack.Push(6);
+
+            return stack;
+        }
+
+    }
+}
